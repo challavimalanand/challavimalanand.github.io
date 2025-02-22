75f6a5501f77f3e7c3f32831860312daa5bc2dad
sramesh89 <sramesh89@gatech.edu>
2025-02-22 10:01:45 -0500

    new theme

---
+++ user-data/data.js
@@ -0,0 +1,115 @@
+export const bio = [
+  "Hello, I'm Challa Vimaland",
+  "<strong> Advocate at High Court </strong> with 4+ years of experience at Telangana High Court",
+];
+
+export const skills = [
+  "Advocacy",
+  "Patents",
+  "Intellectual Property"
+];
+
+export const experience = [
+  {
+    title: " Advocate at Telangana High Court ",
+    duration: "April 2022 - Jul 2023",
+    subtitle: "Software Engineer",
+    details: [
+      "Expert in Civil and Criminal Suites",
+      "Intellectual Property",
+      "Negotiation and Settlements",
+    ],
+    tags: [
+      " Civil",
+      " Criminal",
+      " Patents"
+    ],
+    icon: "heartbeat",
+  },
+
+];
+
+export const education = [
+  {
+    title: "Bar at Law",
+    duration: "2021 - 2024",
+    subtitle: " Law University at Hyd",
+    details: [],
+    tags: [],
+    icon: "graduation-cap",
+  },
+  {
+    title: "MBA in Marketing",
+    duration: "2014 - 2016",
+    subtitle: "IGNOU",
+    details: [],
+    tags: [],
+    icon: "graduation-cap",
+  },
+  {
+    title: "BTech in Mechanical Engineering",
+    duration: "2007-2011",
+    subtitle: "JNTU, Hyderabad",
+    details: [],
+    tags: [],
+    icon: "book",
+  },
+];
+
+export const footer = [
+  //   {
+  //     label: "Dev Profiles",
+  //     data: [
+  //       {
+  //         text: "Stackoverflow",
+  //         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+  //       },
+  //       {
+  //         text: "GitHub",
+  //         link: "https://github.com/vinaysomawat",
+  //       },
+  //       {
+  //         text: "LeetCode",
+  //         link: "https://leetcode.com/somawatvinay/",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     label: "Resources",
+  //     data: [
+  //       {
+  //         text: "Enable Dark/Light Mode",
+  //         func: "enableDarkMode()",
+  //       },
+  //       {
+  //         text: "Print this page",
+  //         func: "window.print()",
+  //       },
+  //       {
+  //         text: "Clone this page",
+  //         link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+  //       },
+  //     ],
+  //   },
+  {
+    label: "Links",
+    data: [
+      //   {
+      //     text: "Linkedin",
+      //     link: "https://www.linkedin.com/in/vinaysomawat/",
+      //   },
+      //   {
+      //     text: "Twitter",
+      //     link: "https://twitter.com/thesigmakid",
+      //   },
+      {
+        text: "Github",
+        link: "https://github.com/challavimalanand/challavimalanand.github.io",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: [" Advocate Challa Vimalanand "],
+  },
+];
