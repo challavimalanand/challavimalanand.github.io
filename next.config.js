f8c834c66eeefc8b6e3722dac420eb5d5b7a9fd0
sidramesh2024 <sidramesh2024@gmail.com>
2025-06-14 00:45:54 -0400

    Fixed relative path

--- next.config.js
+++ next.config.js
@@ -1,24 +1,10 @@
-
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/advocate_portfolio',
-  assetPrefix: '/advocate_portfolio',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { 
-    unoptimized: true 
-  },
-};
+  images: {
+    unoptimized: true
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
